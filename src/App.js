9e40019bb50cf3a0338bce833847512a47ffa328
yassirelkhaili <elkhailiyassir@gmail.com>
2022-12-13 00:47:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { marked } from "marked";
 
-function App() {
+const App = () => {
+  marked.setOptions({
+    breaks: true,
+  });
+  const [input, setinput] = useState(`
+# H1
+## H2
+[title](https://www.example.com)
+\`code\`
+\`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+- First item
+- Second item
+- Third item
+> blockquote
+![alt text](image.jpg)
+**bold text**
+  `);
+  const handleChange = (event) => {
+    setinput(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <div className="header text-center">
+        <h1>MarkdownPreviewer</h1>
+      </div>
+      <div className="row mt-3 d-flex justify-content-center align-items-center form-group">
+        <div className="col-6">
+          <label for="editor">
+            <h6>Enter the Markdown:</h6>
+          </label>
+          <textarea
+            className="form-control"
+            id="editor"
+            onChange={handleChange}
+            value={input}
+          ></textarea>
+        </div>
+        <div className="col-6">
+          <label for="preview">
+            <h6>Result:</h6>
+          </label>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(input) }}
+          ></div>
+        </div>
+        <div></div>
+      </div>
+      <div className="cheatsheet">
+        <a href="https://www.markdownguide.org/cheat-sheet/">
+          Need help? use this cheatsheet<br></br> Brauchen Sie hilfe? setze diesen cheatsheet ein
         </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
